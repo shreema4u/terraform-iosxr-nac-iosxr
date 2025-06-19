c1513225e858da7e1eebc3dda79d67301ee2896a
Robert Crowe (rocrowe) <rocrowe@cisco.com>
2025-06-19 15:51:14 -0400

    Create versions.tf

---
+++ versions.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = ">= 1.8.0"
+
+  required_providers {
+    iosxr = {
+      source  = "CiscoDevNet/iosxr"
+      version = ">= 0.5.3"
+    }
+    utils = {
+      source  = "netascode/utils"
+      version = ">= 0.2.6"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = ">= 2.3.0"
+    }
+  }
+}
