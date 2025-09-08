ec360d8e50b83f608c4c1f2b778e598ee1ed100b
Robert Crowe <rwcrowe@users.noreply.github.com>
2025-09-08 13:19:09 -0400

    refactor(iosxr_interface) interface group configuration handling (#16)
    - Renamed local variable from `device_interfaces` to `device_interfaces_group` for clarity.
    - Introduced a new structure to merge interface configurations using `yaml_merge` and `yamldecode`.
    - Updated the `device_config` and `interface_groups_config` locals to streamline configuration processing.
    - Enhanced readability and maintainability of the interface configuration logic.
    
    Co-authored-by: rocrowe <rocrowe@cisco.com>

--- main.tf
+++ main.tf
@@ -1,10 +1,19 @@
 locals {
-  iosxr         = try(local.model.iosxr, {})
-  global        = try(local.iosxr.global, [])
-  devices       = try(local.iosxr.devices, [])
-  device_groups = try(local.iosxr.device_groups, [])
-  //  interface_groups        = try(local.iosxr.interface_groups, [])
+  iosxr                   = try(local.model.iosxr, {})
+  global                  = try(local.iosxr.global, [])
+  devices                 = try(local.iosxr.devices, [])
+  device_groups           = try(local.iosxr.device_groups, [])
+  interface_groups        = try(local.iosxr.interface_groups, [])
   configuration_templates = try(local.iosxr.configuration_templates, [])
+
+  device_variables = { for device in local.devices :
+    device.name => merge(concat(
+      [try(local.global.variables, {})],
+      [for dg in local.device_groups : try(dg.variables, {}) if contains(try(device.device_groups, []), dg.name)],
+      [for dg in local.device_groups : try(dg.variables, {}) if contains(try(dg.devices, []), device.name)],
+      [try(device.variables, {})]
+    )...)
+  }
 
   device_group_config_template_variables = { for dg in local.device_groups :
     dg.name => merge(concat(
@@ -23,7 +32,7 @@ locals {
     dg => templatestring(config, local.device_group_config_template_variables[dg])
   }
 
-  raw_device_config = { for device in local.devices :
+  device_raw_config = { for device in local.devices :
     device.name => try(provider::utils::yaml_merge(concat(
       [yamlencode(try(local.global.configuration, {}))],
       [for dg in local.device_groups : yamlencode(try(dg.configuration, {})) if contains(try(device.device_groups, []), dg.name)],
@@ -34,17 +43,23 @@ locals {
     )), "")
   }
 
-  device_variables = { for device in local.devices :
-    device.name => merge(concat(
-      [try(local.global.variables, {})],
-      [for dg in local.device_groups : try(dg.variables, {}) if contains(try(device.device_groups, []), dg.name)],
-      [for dg in local.device_groups : try(dg.variables, {}) if contains(try(dg.devices, []), device.name)],
-      [try(device.variables, {})]
-    )...)
+  device_config = { for device, config in local.device_raw_config :
+    device => yamldecode(templatestring(config, local.device_variables[device]))
   }
 
-  device_config = { for device, config in local.raw_device_config :
-    device => yamldecode(templatestring(config, local.device_variables[device]))
+  interface_groups_raw_config = {
+    for device in local.devices : device.name => {
+      for ig in local.interface_groups : ig.name => yamlencode(try(ig.configuration, {}))
+    }
+  }
+
+  interface_groups_config = {
+    for device in local.devices : device.name => [
+      for ig in local.interface_groups : {
+        name          = ig.name
+        configuration = yamldecode(templatestring(local.interface_groups_raw_config[device.name][ig.name], local.device_variables[device.name]))
+      }
+    ]
   }
 
   provider_devices = [for device in local.devices : {
