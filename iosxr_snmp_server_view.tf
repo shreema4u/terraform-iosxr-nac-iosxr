3dcd5e90735faf74a2a6f92a870c0a81f73a4880
RaviPatra <136333381+RaviPatra@users.noreply.github.com>
2025-09-10 22:24:00 +0530

    Implemented iosxr_snmp_server_views feature (#17)
    * Updated with banner feature.
    
    * updated the variable file to empty list
    
    * implemented iosxr_cdp feature.
    
    * Removed unused files.
    Updated files with default data.
    
    * Delete examples/system/system.nac.yaml
    
    * Updated pre-commit file.
    
    * Updated Readme file using pre-commit command
    
    * Address Git test failed issues by adding system.nac.yaml file.
    
    * Replaced system.nac.yaml with master copy.
    
    * implemented iosxr_snmp_server_view feature.
    
    * implemented iosxr_snmp_server_view feature.
    
    * Handled with try and default value.
    
    * Addressed code review comments
    
    * Updated readme file
    
    ---------
    
    Co-authored-by: rpatraga <>
    Co-authored-by: Robert Crowe <rwcrowe@users.noreply.github.com>

---
+++ iosxr_snmp_server_view.tf
@@ -0,0 +1,26 @@
+locals {
+  device_snmp_server_view_configs = flatten([
+    for device in local.devices : [
+      for view in try(local.device_config[device.name].snmp_server_views, local.defaults.iosxr.configuration.snmp_server_views, []) : {
+        device_name = device.name
+        key         = try("${device.name}-snmp-server-view-${view.view_name}", null)
+        view_name   = try(view.view_name, local.defaults.iosxr.configuration.snmp_server_view_name, null)
+        mib_view_families = [
+          for family in try(view.mib_view_families, local.defaults.iosxr.configuration.snmp_server_view_mib_view_families, []) : {
+            name     = try(family.name, local.defaults.iosxr.configuration.snmp_server_view_mib_view_family_name, null)
+            excluded = try(family.excluded, local.defaults.iosxr.configuration.snmp_server_view_mib_view_family_excluded, null)
+            included = try(family.included, local.defaults.iosxr.configuration.snmp_server_view_mib_view_family_included, null)
+          }
+        ]
+      }
+    ] if try(local.device_config[device.name].snmp_server_views, local.defaults.iosxr.configuration.snmp_server_views, null) != null
+  ])
+}
+
+resource "iosxr_snmp_server_view" "snmp_server_view" {
+  for_each = { for view_config in local.device_snmp_server_view_configs : view_config.key => view_config }
+  device   = each.value.device_name
+
+  view_name         = each.value.view_name
+  mib_view_families = each.value.mib_view_families
+}
