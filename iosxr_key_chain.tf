f741841b3ada2d3bd5c2abf65c474d2e5e001920
Robert Crowe <rwcrowe@users.noreply.github.com>
2025-09-24 14:08:57 -0400

    refactor(iosxr_key_chain): use null values instead of list (#37)
    * refactor(iosxr_key_chain): correct key chain list defaults handling
    
    * refactor(iosxr_key_chain): correct key chain list defaults handling
    
    * refactor(iosxr_key_chain): use null values instead of list
    
    - removed defaults for lists
    
    ---------
    
    Co-authored-by: rocrowe <rocrowe@cisco.com>

--- iosxr_key_chain.tf
+++ iosxr_key_chain.tf
@@ -1,31 +1,29 @@
 locals {
   device_key_chains = flatten([
     for device in local.devices : [
-      for key_chain in try(local.device_config[device.name].key_chain, local.defaults.iosxr.configuration.key_chain, []) : {
+      for key_chain in try(local.device_config[device.name].key_chain, []) : {
         device_name = device.name
-        name        = try(key_chain.name, local.defaults.iosxr.configuration.key_chain_name, null)
+        name        = try(key_chain.name, local.defaults.iosxr.configuration.key_chain.name, null)
         key         = "${device.name}-keychain-${key_chain.name}"
-        keys = [
-          for key in try(key_chain.keys, local.defaults.iosxr.configuration.key_chain_keys, []) : {
-            key_name                                = try(key.key_name, local.defaults.iosxr.configuration.key_chain_key_name, null)
-            key_string_password                     = try(key.key_string_password, local.defaults.iosxr.configuration.key_chain_key_string_password, null)
-            cryptographic_algorithm                 = try(key.cryptographic_algorithm, local.defaults.iosxr.configuration.key_chain_cryptographic_algorithm, null)
-            accept_lifetime_infinite                = try(key.accept_lifetime_infinite, local.defaults.iosxr.configuration.key_chain_accept_lifetime_infinite, null)
-            accept_lifetime_start_time_hour         = try(key.accept_lifetime_start_time_hour, local.defaults.iosxr.configuration.key_chain_accept_lifetime_start_time_hour, null)
-            accept_lifetime_start_time_minute       = try(key.accept_lifetime_start_time_minute, local.defaults.iosxr.configuration.key_chain_accept_lifetime_start_time_minute, null)
-            accept_lifetime_start_time_second       = try(key.accept_lifetime_start_time_second, local.defaults.iosxr.configuration.key_chain_accept_lifetime_start_time_second, null)
-            accept_lifetime_start_time_day_of_month = try(key.accept_lifetime_start_time_day_of_month, local.defaults.iosxr.configuration.key_chain_accept_lifetime_start_time_day_of_month, null)
-            accept_lifetime_start_time_month        = try(key.accept_lifetime_start_time_month, local.defaults.iosxr.configuration.key_chain_accept_lifetime_start_time_month, null)
-            accept_lifetime_start_time_year         = try(key.accept_lifetime_start_time_year, local.defaults.iosxr.configuration.key_chain_accept_lifetime_start_time_year, null)
-            send_lifetime_infinite                  = try(key.send_lifetime_infinite, local.defaults.iosxr.configuration.key_chain_send_lifetime_infinite, null)
-            send_lifetime_start_time_hour           = try(key.send_lifetime_start_time_hour, local.defaults.iosxr.configuration.key_chain_send_lifetime_start_time_hour, null)
-            send_lifetime_start_time_minute         = try(key.send_lifetime_start_time_minute, local.defaults.iosxr.configuration.key_chain_send_lifetime_start_time_minute, null)
-            send_lifetime_start_time_second         = try(key.send_lifetime_start_time_second, local.defaults.iosxr.configuration.key_chain_send_lifetime_start_time_second, null)
-            send_lifetime_start_time_day_of_month   = try(key.send_lifetime_start_time_day_of_month, local.defaults.iosxr.configuration.key_chain_send_lifetime_start_time_day_of_month, null)
-            send_lifetime_start_time_month          = try(key.send_lifetime_start_time_month, local.defaults.iosxr.configuration.key_chain_send_lifetime_start_time_month, null)
-            send_lifetime_start_time_year           = try(key.send_lifetime_start_time_year, local.defaults.iosxr.configuration.key_chain_send_lifetime_start_time_year, null)
-          }
-        ]
+        keys = try(length(key_chain.keys) == 0, true) ? null : [for key in key_chain.keys : {
+          key_name                                = try(key.key_name, local.defaults.iosxr.configuration.key_chain.keys.key_name, null)
+          key_string_password                     = try(key.key_string_password, local.defaults.iosxr.configuration.key_chain.keys.key_string_password, null)
+          cryptographic_algorithm                 = try(key.cryptographic_algorithm, local.defaults.iosxr.configuration.key_chain.keys.cryptographic_algorithm, null)
+          accept_lifetime_infinite                = try(key.accept_lifetime_infinite, local.defaults.iosxr.configuration.key_chain.keys.accept_lifetime_infinite, null)
+          accept_lifetime_start_time_hour         = try(key.accept_lifetime_start_time_hour, local.defaults.iosxr.configuration.key_chain.keys.accept_lifetime_start_time_hour, null)
+          accept_lifetime_start_time_minute       = try(key.accept_lifetime_start_time_minute, local.defaults.iosxr.configuration.key_chain.keys.accept_lifetime_start_time_minute, null)
+          accept_lifetime_start_time_second       = try(key.accept_lifetime_start_time_second, local.defaults.iosxr.configuration.key_chain.keys.accept_lifetime_start_time_second, null)
+          accept_lifetime_start_time_day_of_month = try(key.accept_lifetime_start_time_day_of_month, local.defaults.iosxr.configuration.key_chain.keys.accept_lifetime_start_time_day_of_month, null)
+          accept_lifetime_start_time_month        = try(key.accept_lifetime_start_time_month, local.defaults.iosxr.configuration.key_chain.keys.accept_lifetime_start_time_month, null)
+          accept_lifetime_start_time_year         = try(key.accept_lifetime_start_time_year, local.defaults.iosxr.configuration.key_chain.keys.accept_lifetime_start_time_year, null)
+          send_lifetime_infinite                  = try(key.send_lifetime_infinite, local.defaults.iosxr.configuration.key_chain.keys.send_lifetime_infinite, null)
+          send_lifetime_start_time_hour           = try(key.send_lifetime_start_time_hour, local.defaults.iosxr.configuration.key_chain.keys.send_lifetime_start_time_hour, null)
+          send_lifetime_start_time_minute         = try(key.send_lifetime_start_time_minute, local.defaults.iosxr.configuration.key_chain.keys.send_lifetime_start_time_minute, null)
+          send_lifetime_start_time_second         = try(key.send_lifetime_start_time_second, local.defaults.iosxr.configuration.key_chain.keys.send_lifetime_start_time_second, null)
+          send_lifetime_start_time_day_of_month   = try(key.send_lifetime_start_time_day_of_month, local.defaults.iosxr.configuration.key_chain.keys.send_lifetime_start_time_day_of_month, null)
+          send_lifetime_start_time_month          = try(key.send_lifetime_start_time_month, local.defaults.iosxr.configuration.key_chain.keys.send_lifetime_start_time_month, null)
+          send_lifetime_start_time_year           = try(key.send_lifetime_start_time_year, local.defaults.iosxr.configuration.key_chain.keys.send_lifetime_start_time_year, null)
+        }]
       }
     ]
     if try(local.device_config[device.name].key_chain, null) != null || try(local.defaults.iosxr.configuration.key_chain, null) != null
