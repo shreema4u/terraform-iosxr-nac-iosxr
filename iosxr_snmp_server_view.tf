6bc293461041f15857fed2eaa4531ec4e9e2904d
RaviPatra <136333381+RaviPatra@users.noreply.github.com>
2025-09-25 00:31:35 +0530

    Updated iosxr_snmp_server_views feature (#36)
    * Updated with banner feature.
    
    * updated the variable file to empty list
    
    * implemented iosxr_cdp feature.
    
    * Removed unused files.
    Updated files with default data.
    
    * Delete examples/system/system.nac.yaml
    
    * Updated pre-commit file.
    
    * Updated Readme file using pre-commit command
    
    * Address Git test failed issues by adding system.nac.yaml file.
    
    * Replaced system.nac.yaml with master copy.
    
    * Updated to snmp_server_view
    
    * Addressed code review comments.
    
    * Addressed code review comments.
    
    * Update iosxr_snmp_server_view.tf
    
    ---------
    
    Co-authored-by: rpatraga <>
    Co-authored-by: Robert Crowe <rwcrowe@users.noreply.github.com>

--- iosxr_snmp_server_view.tf
+++ iosxr_snmp_server_view.tf
@@ -1,15 +1,15 @@
 locals {
   device_snmp_server_view_configs = flatten([
     for device in local.devices : [
-      for view in try(local.device_config[device.name].snmp_server_view, local.defaults.iosxr.configuration.snmp_server_view, []) : {
+      for view in try(local.device_config[device.name].snmp_server_view, []) : {
         device_name = device.name
-        key         = try("${device.name}-snmp-server-view-${view.view_name}", null)
-        view_name   = try(view.view_name, local.defaults.iosxr.configuration.snmp_server_view_name, null)
+        key         = "${device.name}-snmp-server-view-${view.view_name}"
+        view_name   = try(view.view_name, local.defaults.iosxr.configuration.snmp_server_view.view_name, null)
         mib_view_families = [
-          for family in try(view.mib_view_families, local.defaults.iosxr.configuration.snmp_server_view_mib_view_families, []) : {
-            name     = try(family.name, local.defaults.iosxr.configuration.snmp_server_view_mib_view_family_name, null)
-            excluded = try(family.excluded, local.defaults.iosxr.configuration.snmp_server_view_mib_view_family_excluded, null)
-            included = try(family.included, local.defaults.iosxr.configuration.snmp_server_view_mib_view_family_included, null)
+          for family in try(view.mib_view_families, local.defaults.iosxr.configuration.snmp_server_view.mib_view_families, []) : {
+            name     = try(family.name, local.defaults.iosxr.configuration.snmp_server_view.mib_view_families.name, null)
+            excluded = try(family.excluded, local.defaults.iosxr.configuration.snmp_server_view.mib_view_families.excluded, null)
+            included = try(family.included, local.defaults.iosxr.configuration.snmp_server_view.mib_view_families.included, null)
           }
         ]
       }
