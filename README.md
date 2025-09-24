2feea3508e4f7f72e163579275a4a3e4c5deec7f
shashidhar184 <148075426+shashidhar184@users.noreply.github.com>
2025-09-24 19:40:29 +0530

    Implemented iosxr_l2vpn feature (#28)
    * Commit this 2 files
    
    * Updated the fix
    
    * Updated iosxr_l2vpn.tf
    
    * Updated the fix
    
    * Update iosxr_l2vpn.tf
    
    ---------
    
    Co-authored-by: Robert Crowe <rwcrowe@users.noreply.github.com>

--- README.md
+++ README.md
@@ -68,6 +68,7 @@ module "iosxr" {
 | [iosxr_interface.interface](https://registry.terraform.io/providers/CiscoDevNet/iosxr/latest/docs/resources/interface) | resource |
 | [iosxr_ipv6_access_list_options.ipv6_access_list_options](https://registry.terraform.io/providers/CiscoDevNet/iosxr/latest/docs/resources/ipv6_access_list_options) | resource |
 | [iosxr_key_chain.key_chain](https://registry.terraform.io/providers/CiscoDevNet/iosxr/latest/docs/resources/key_chain) | resource |
+| [iosxr_l2vpn.l2vpn](https://registry.terraform.io/providers/CiscoDevNet/iosxr/latest/docs/resources/l2vpn) | resource |
 | [iosxr_l2vpn_bridge_group.l2vpn_bridge_group](https://registry.terraform.io/providers/CiscoDevNet/iosxr/latest/docs/resources/l2vpn_bridge_group) | resource |
 | [iosxr_lldp.lldp](https://registry.terraform.io/providers/CiscoDevNet/iosxr/latest/docs/resources/lldp) | resource |
 | [iosxr_logging.logging](https://registry.terraform.io/providers/CiscoDevNet/iosxr/latest/docs/resources/logging) | resource |
---
+++ iosxr_l2vpn.tf
@@ -0,0 +1,33 @@
+locals {
+  device_l2vpn_configs = flatten([
+    for device in local.devices : [
+      {
+        device_name = device.name
+        key         = "${device.name}-l2vpn"
+
+        description                     = try(local.device_config[device.name].l2vpn.description, local.defaults.iosxr.configuration.l2vpn_description, null)
+        router_id                       = try(local.device_config[device.name].l2vpn.router_id, local.defaults.iosxr.configuration.l2vpn_router_id, null)
+        load_balancing_flow_src_dst_ip  = try(local.device_config[device.name].l2vpn.load_balancing_flow_src_dst_ip, local.defaults.iosxr.configuration.l2vpn_load_balancing_flow_src_dst_ip, null)
+        load_balancing_flow_src_dst_mac = try(local.device_config[device.name].l2vpn.load_balancing_flow_src_dst_mac, local.defaults.iosxr.configuration.l2vpn_load_balancing_flow_src_dst_mac, null)
+
+        xconnect_groups = [
+          for group in try(local.device_config[device.name].l2vpn.xconnect_groups, local.defaults.iosxr.configuration.l2vpn_xconnect_groups, []) : {
+            group_name = try(group.group_name, null)
+          }
+        ]
+      }
+    ]
+    if try(local.device_config[device.name].l2vpn, null) != null || try(local.defaults.iosxr.configuration.l2vpn, null) != null
+  ])
+}
+
+resource "iosxr_l2vpn" "l2vpn" {
+  for_each = { for l2vpn in local.device_l2vpn_configs : l2vpn.key => l2vpn }
+
+  device                          = each.value.device_name
+  description                     = each.value.description
+  router_id                       = each.value.router_id
+  load_balancing_flow_src_dst_ip  = each.value.load_balancing_flow_src_dst_ip
+  load_balancing_flow_src_dst_mac = each.value.load_balancing_flow_src_dst_mac
+  xconnect_groups                 = each.value.xconnect_groups
+}
