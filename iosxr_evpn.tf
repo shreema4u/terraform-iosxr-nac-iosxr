d7adfdd6a90635cdf974aee7d640ef74ec278612
Robert Crowe <rwcrowe@users.noreply.github.com>
2025-09-30 19:29:56 -0400

    refactor(iosxr_evpn): change evpn resource to simple resource (#49)
    Co-authored-by: rocrowe <rocrowe@cisco.com>

--- iosxr_evpn.tf
+++ iosxr_evpn.tf
@@ -1,19 +1,6 @@
-locals {
-  device_evpn = flatten([
-    for device in local.devices : [
-      {
-        device_name      = device.name
-        key              = "${device.name}-evpn"
-        source_interface = try(local.device_config[device.name].evpn.source_interface, local.defaults.iosxr.configuration.evpn.source_interface, null)
-      }
-    ]
-    if try(local.device_config[device.name].evpn, null) != null || try(local.defaults.iosxr.configuration.evpn, null) != null
-  ])
-}
-
 resource "iosxr_evpn" "evpn" {
-  for_each = { for evpn in local.device_evpn : evpn.key => evpn }
+  for_each = { for device in local.devices : device.name => device if try(local.device_config[device.name].evpn, null) != null || try(local.defaults.iosxr.configuration.evpn, null) != null }
+  device   = each.value.name
 
-  device           = each.value.device_name
-  source_interface = each.value.source_interface
+  source_interface = try(local.device_config[each.value.name].evpn.source_interface, local.defaults.iosxr.configuration.evpn.source_interface, null)
 }
