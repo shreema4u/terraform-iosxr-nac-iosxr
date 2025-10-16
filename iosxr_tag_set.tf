42a94f7258f913673f549953dfef60047b398ea6
rhuseyn <rhuseyno@cisco.com>
2025-10-16 17:53:41 +0200

    [ENHANCEMENT] Add support for tag_set #148  (#56)
    * readme and tag_set.tf
    
    * [netascode/terraform-iosxr-nac-iosxr] [ENHANCEMENT] Add support for tag_set #148 (PR #56)
    
    ---------
    
    Co-authored-by: rasimh542 <rasimh542@cisco.com>

---
+++ iosxr_tag_set.tf
@@ -0,0 +1,21 @@
+locals {
+  device_tag_sets = flatten([
+    for device in local.devices : [
+      for tag_set in try(local.device_config[device.name].tag_sets, []) : {
+        device_name = device.name
+        set_name    = try(tag_set.set_name, local.defaults.iosxr.configuration.tag_sets.set_name, null)
+        rpl_tag_set = try(tag_set.rpl, local.defaults.iosxr.configuration.tag_sets.rpl, null)
+        key         = "${device.name}-${tag_set.set_name}"
+      }
+    ]
+    if try(local.device_config[device.name].tag_sets, null) != null && try(local.defaults.iosxr.configuration.tag_sets, null) != null
+  ])
+}
+
+resource "iosxr_tag_set" "tag_set" {
+  for_each = { for tag_set in local.device_tag_sets : tag_set.key => tag_set }
+
+  device      = each.value.device_name
+  set_name    = each.value.set_name
+  rpl_tag_set = each.value.rpl_tag_set
+}
