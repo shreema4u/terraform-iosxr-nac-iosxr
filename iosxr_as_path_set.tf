e0cb2d545bb9e79a5dff89b65612d77532ba99f1
Robert Crowe <rwcrowe@users.noreply.github.com>
2025-10-23 16:39:08 -0400

    refactor(as-path-sets): update local variable names for consistency (#90)
    Co-authored-by: rocrowe <rocrowe@cisco.com>

--- iosxr_as_path_set.tf
+++ iosxr_as_path_set.tf
@@ -1,20 +1,18 @@
 locals {
-  device_as_path_sets = flatten([
+  as_path_sets = flatten([
     for device in local.devices : [
-      for as_path_set in try(local.device_config[device.name].as_path_set, []) : {
+      for as_path_set in try(local.device_config[device.name].as_path_sets, []) : {
         key         = "${device.name}-${as_path_set.set_name}"
         device_name = device.name
-        set_name    = try(as_path_set.set_name, local.defaults.iosxr.configuration.as_path_set.set_name, null)
-        rpl         = try(as_path_set.rpl, local.defaults.iosxr.configuration.as_path_set.rpl, null)
+        set_name    = try(as_path_set.set_name, local.defaults.iosxr.configuration.as_path_sets.set_name, null)
+        rpl         = try(as_path_set.rpl, local.defaults.iosxr.configuration.as_path_sets.rpl, null)
       }
-      if try(as_path_set.set_name, local.defaults.iosxr.configuration.as_path_set.set_name, null) != null &&
-      try(as_path_set.rpl, local.defaults.iosxr.configuration.as_path_set.rpl, null) != null
     ]
   ])
 }
 
 resource "iosxr_as_path_set" "as_path_set" {
-  for_each = { for as_path_set in local.device_as_path_sets : as_path_set.key => as_path_set }
+  for_each = { for as_path_set in local.as_path_sets : as_path_set.key => as_path_set }
   device   = each.value.device_name
   set_name = each.value.set_name
   rpl      = each.value.rpl
