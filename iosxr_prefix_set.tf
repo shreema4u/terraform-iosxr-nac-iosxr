007598c0c09925d559ab6aa3f2b488b10d5a2fba
Robert Crowe <rwcrowe@users.noreply.github.com>
2025-10-23 14:16:30 -0400

    refactor(prefix-sets): rename local variables (#89)
    - Updated local variable names to align with the new configuration structure.
    - Removed redundant conditionals for improved readability.
    
    Co-authored-by: rocrowe <rocrowe@cisco.com>

--- iosxr_prefix_set.tf
+++ iosxr_prefix_set.tf
@@ -1,20 +1,18 @@
 locals {
-  device_prefix_sets = flatten([
+  prefix_sets = flatten([
     for device in local.devices : [
-      for prefix_set in try(local.device_config[device.name].prefix_set, []) : {
+      for prefix_set in try(local.device_config[device.name].prefix_sets, []) : {
         key         = "${device.name}-${prefix_set.set_name}"
         device_name = device.name
-        set_name    = try(prefix_set.set_name, local.defaults.iosxr.configuration.prefix_set.set_name, null)
-        rpl         = try(prefix_set.rpl, local.defaults.iosxr.configuration.prefix_set.rpl, null)
+        set_name    = try(prefix_set.set_name, local.defaults.iosxr.configuration.prefix_sets.set_name, null)
+        rpl         = try(prefix_set.rpl, local.defaults.iosxr.configuration.prefix_sets.rpl, null)
       }
-      if try(prefix_set.set_name, local.defaults.iosxr.configuration.prefix_set.set_name, null) != null &&
-      try(prefix_set.rpl, local.defaults.iosxr.configuration.prefix_set.rpl, null) != null
     ]
   ])
 }
 
 resource "iosxr_prefix_set" "prefix_set" {
-  for_each = { for prefix_set in local.device_prefix_sets : prefix_set.key => prefix_set }
+  for_each = { for prefix_set in local.prefix_sets : prefix_set.key => prefix_set }
   device   = each.value.device_name
   set_name = each.value.set_name
   rpl      = each.value.rpl
