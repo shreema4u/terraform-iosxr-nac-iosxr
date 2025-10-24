0d7183ae2ddde0a388ea52e46bd7fa0a271a0f23
Robert Crowe <rwcrowe@users.noreply.github.com>
2025-10-24 17:04:23 -0400

    refactor(evpn-groups): update local variable names for consistency (#94)
    Co-authored-by: rocrowe <rocrowe@cisco.com>

--- iosxr_evpn_group.tf
+++ iosxr_evpn_group.tf
@@ -1,12 +1,12 @@
 locals {
   evpn_groups = flatten([
     for device in local.devices : [
-      for evpn_group in try(local.device_config[device.name].evpn_group, []) : {
+      for evpn_group in try(local.device_config[device.name].evpn_groups, []) : {
         key         = "${device.name}-${evpn_group.group_id}"
         device_name = device.name
-        group_id    = try(evpn_group.group_id, local.defaults.iosxr.configuration.evpn_group.group_id, null)
+        group_id    = try(evpn_group.group_id, local.defaults.iosxr.configuration.evpn_groups.group_id, null)
         core_interfaces = try(length(evpn_group.core_interfaces) == 0, true) ? null : [for core_interface in evpn_group.core_interfaces : {
-          interface_name = try(core_interface.interface_name, local.defaults.iosxr.configuration.evpn_group.core_interfaces.interface_name, null)
+          interface_name = try(core_interface.interface_name, local.defaults.iosxr.configuration.evpn_groups.core_interfaces.interface_name, null)
         }]
       }
     ]
