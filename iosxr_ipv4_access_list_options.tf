47934bf8bdb0a835760d0666739f9fe720b53333
Robert Crowe <rwcrowe@users.noreply.github.com>
2026-01-29 18:07:07 -0500

    add icmp_off (#117)
    Co-authored-by: rocrowe <rocrowe@cisco.com>

--- iosxr_ipv4_access_list_options.tf
+++ iosxr_ipv4_access_list_options.tf
@@ -1,6 +1,7 @@
 resource "iosxr_ipv4_access_list_options" "ipv4_access_list_options" {
   for_each             = { for device in local.devices : device.name => device if try(local.device_config[device.name].ipv4_access_list_options, null) != null || try(local.defaults.iosxr.devices.configuration.ipv4_access_list_options, null) != null }
   device               = each.value.name
+  icmp_off             = try(local.device_config[each.value.name].ipv4_access_list_options.icmp_off, local.defaults.iosxr.devices.configuration.ipv4_access_list_options.icmp_off, null)
   log_update_rate      = try(local.device_config[each.value.name].ipv4_access_list_options.log_update_rate, local.defaults.iosxr.devices.configuration.ipv4_access_list_options.log_update_rate, null)
   log_update_threshold = try(local.device_config[each.value.name].ipv4_access_list_options.log_update_threshold, local.defaults.iosxr.devices.configuration.ipv4_access_list_options.log_update_threshold, null)
 }
