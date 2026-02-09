f5713d07cd1500af58da4904809f167d727fd25e
rocrowe <rocrowe@cisco.com>
2026-02-09 16:52:48 -0500

    fixes

--- iosxr_radius_source_interface.tf
+++ iosxr_radius_source_interface.tf
@@ -1,20 +1,18 @@
 locals {
-  device_radius_source_interface = flatten([
+  radius_source_interfaces = flatten([
     for device in local.devices : [
       for radius_source_interface in try(local.device_config[device.name].radius_source_interface, []) : {
+        key              = format("%s/%s", device.name, radius_source_interface.vrf)
         device_name      = device.name
-        key              = "${device.name}-${radius_source_interface.vrf}"
         vrf              = radius_source_interface.vrf
-        source_interface = try(radius_source_interface.source_interface, local.defaults.iosxr.configuration.radius_source_interface.source_interface, null)
+        source_interface = try(radius_source_interface.source_interface, local.defaults.iosxr.devices.configuration.radius_source_interface.source_interface, null)
       }
     ]
-    if try(local.device_config[device.name].radius_source_interface, null) != null
   ])
 }
 
 resource "iosxr_radius_source_interface" "radius_source_interface" {
-  for_each = { for item in local.device_radius_source_interface : item.key => item }
-
+  for_each         = { for radius_source_interface in local.radius_source_interfaces : radius_source_interface.key => radius_source_interface }
   device           = each.value.device_name
   vrf              = each.value.vrf
   source_interface = each.value.source_interface
