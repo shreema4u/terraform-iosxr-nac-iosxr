393da9b428415cff930f10cd2d89cfaea082aa45
rocrowe <rocrowe@cisco.com>
2026-02-10 12:59:45 -0500

    add depends

--- iosxr_route_policy.tf
+++ iosxr_route_policy.tf
@@ -18,6 +18,7 @@ resource "iosxr_route_policy" "route_policy" {
   rpl               = each.value.rpl
 
   depends_on = [
+    iosxr_key_chain.key_chain,
     iosxr_as_path_set.as_path_set,
     iosxr_community_set.community_set,
     iosxr_esi_set.esi_set,
@@ -25,11 +26,11 @@ resource "iosxr_route_policy" "route_policy" {
     iosxr_rd_set.rd_set,
     iosxr_tag_set.tag_set,
     iosxr_extcommunity_cost_set.extcommunity_cost_set,
-    iosxr_extcommunity_opaque_set.extcommunity_opaque_set
+    iosxr_extcommunity_opaque_set.extcommunity_opaque_set,
     # Future dependencies - uncomment when resource is created:
     #iosxr_extcommunity_rt_set.extcommunity_rt_set,
     #iosxr_extcommunity_soo_set.extcommunity_soo_set,
-    #iosxr_ipv4_prefix_list.ipv4_prefix_list,
+    iosxr_ipv4_prefix_list.ipv4_prefix_list
     #iosxr_ipv6_prefix_list.ipv6_prefix_list,
     #iosxr_ipv4_access_list.ipv4_access_list,
     #iosxr_ipv6_access_list.ipv6_access_list
